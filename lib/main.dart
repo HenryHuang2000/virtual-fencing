d36b490397a8bf426cbc489bcabfb5c63442dc91
Aniket Chavan <chavaniket360@protonmail.com>
2020-11-12 14:43:57 +1100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+// import 'dart:html';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:http/http.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,13 +33,171 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GeolocationExample(title: 'Geolocation Example'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class GeolocationExampleState extends State<GeolocationExample> {
+  Geolocator _geolocator;
+  Position _position;
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _geolocator = Geolocator();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  bool isLoading = false;
+  List<Address> results = [];
+
+  Future<void> initConnectivity() async {
+    ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+      print(result.toString());
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+
+    switch (result) {
+
+      case ConnectivityResult.mobile:
+        setState(() => _connectionStatus = result.toString());
+        _makePostRequest("Angela", 12345, _position.toString());
+        break;
+      case ConnectivityResult.none:
+        setState(() => _connectionStatus = result.toString());
+        break;
+      case ConnectivityResult.wifi:
+      setState(() => _connectionStatus = result.toString());
+        break;
+      default:
+        setState(() => _connectionStatus = 'Failed to get connectivity.');
+        break;
+    }
+  }
+
+  Future<void> _makePostRequest(name, id, location) async {
+    // set up POST request arguments
+    String url = 'http://754013b77161.ngrok.io/api/records';
+    Map<String, String> headers = {"Content-type": "application/json"};
+    String json = '{"name": "${name}", "userId": "${id}", "location": "${location}"}';
+    // make POST request
+    Response response = await post(url, headers: headers, body: json);
+    // check the status code for the result
+    int statusCode = response.statusCode;
+    print("Post Response Code: " + statusCode.toString());
+    // this API passes back the id of the new item added to the body
+    String body = response.body;
+    print("Post Response Body: " + statusCode.toString());
+
+    // {
+    //   "title": "Hello",
+    //   "body": "body text",
+    //   "userId": 1,
+    //   "id": 101
+    // }
+  }
+
+  void funContainer() {
+    _updateLocation();
+    search(_position.latitude, _position.longitude);
+    _makePostRequest("Leo", 09876, _position.toString());
+  }
+
+  void _updateLocation() async {
+    try {
+      Position newPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+          .timeout(new Duration(seconds: 5));
+
+      setState(() {
+        _position = newPosition;
+      });
+    } catch (e) {
+      print('Error: ${e.toString()}');
+    }
+  }
+
+  Future search(latitude, longitude) async {
+    this.setState(() {
+      this.isLoading = true;
+    });
+
+    try{
+      print(latitude);
+      print(longitude);
+      var results = await Geocoder.google('AIzaSyCEewoe84r5nUHZSYE-o9zJogk5XwP17Mc').findAddressesFromCoordinates(new Coordinates(latitude, longitude));
+      this.setState(() {
+        print("Results: " + results.toString());
+        this.results = results;
+      });
+    }
+    catch(e) {
+      print("Error occurred: $e");
+    }
+    finally {
+      this.setState(() {
+        this.isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Geolocation Example'),
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                    'Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
+                        ' Longitude: ${_position != null ? _position.longitude.toString() : '0'}\n'
+                        'Wifi: ${_connectionStatus}'
+                )
+              ]
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: funContainer,
+        tooltip: 'Get Location',
+        child: Icon(Icons.location_searching_outlined),
+      ),
+    );
+  }
+}
+
+class GeolocationExample extends StatefulWidget {
+  GeolocationExample({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -46,72 +211,5 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  GeolocationExampleState createState() => GeolocationExampleState();
 }
